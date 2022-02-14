f96e26518990c4e2d64f6ad7ada4358da059e8d0
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-14 15:14:00 -0800

    fixed newEntryId increment and listen for beforeunload event on the entry form. #1

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ var $entryForm = document.querySelector('#entry-form');
 
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  var nextEntryId = {};
+  var entryId = {};
 
-  nextEntryId.photoUrl = $entryForm.elements.photoUrl.value;
-  nextEntryId.title = $entryForm.elements.title.value;
-  nextEntryId.notes = $entryForm.elements.notes.value;
-  nextEntryId.nextEntryId = data.newEntryId;
+  entryId.photoUrl = $entryForm.elements.photoUrl.value;
+  entryId.title = $entryForm.elements.title.value;
+  entryId.notes = $entryForm.elements.notes.value;
 
+  entryId.EntryId = data.newEntryId;
   data.newEntryId++;
 
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
