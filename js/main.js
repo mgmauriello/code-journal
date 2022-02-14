5a0737c9b968360adec7859e5f76da565a0a74a1
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-14 13:54:08 -0800

    Listen for the input event on the entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+var $input = document.querySelector('#photoUrl');
+var $img = document.querySelector('img');
+
+$input.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+});
