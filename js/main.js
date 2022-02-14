cbfaaa2a81cceff60ad7740abbf120d872261fa3
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-14 14:33:20 -0800

    Listen for the submit event on the entry form. #1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ var $img = document.querySelector('img');
 $input.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
 });
+
+var $entryForm = document.querySelector('#entry-form');
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var nextEntryId = {};
+
+  nextEntryId.photoUrl = $entryForm.elements.photoUrl.value;
+  nextEntryId.title = $entryForm.elements.title.value;
+  nextEntryId.notes = $entryForm.elements.notes.value;
+  nextEntryId.nextEntryId = data.newEntryId;
+
+  data.newEntryId++;
+
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+
+});
