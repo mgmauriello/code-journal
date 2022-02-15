dbfba653b792492aad2f8cba33e4f05dc21b96fa
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-15 13:55:33 -0800

    prepared for pull request, addition of :hover and :active pseudoclasses. #1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 /* exported data */
 var $input = document.querySelector('#photoUrl');
 var $img = document.querySelector('img');
-var $entriesList = document.querySelector('.entries-list');
 var $entryForm = document.querySelector('#entry-form');
 
 $input.addEventListener('input', function (event) {
@@ -22,53 +21,5 @@ $entryForm.addEventListener('submit', function (event) {
 
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
-});
 
-/*
-users can view their entry #2
-recreate entry form line by line (row (in div), col (in div), then img, divs
-form wont be form - title = h1? entries text = p ;; ^^ look at props
-in the $entrieslist - appendChild
-*/
-
-function viewEntries(entry) {
-  var listofEntries = document.createElement('li');
-
-  var row = document.createElement('div');
-  row.className = 'row';
-  listofEntries.appendChild(row);
-
-  var columnHalf = document.createElement('div');
-  columnHalf.className = 'column-half';
-  row.appendChild(columnHalf);
-
-  var img = document.createElement('img');
-  img.setAttribute('src', entry.photoUrl);
-  img.className = 'img-entries';
-  columnHalf.appendChild(img);
-
-  var entriesText = document.createElement('div');
-  entriesText.className = 'column half entries-text';
-  row.appendChild(entriesText);
-
-  var titles = document.createElement('h2');
-  titles.textContent = entry.title;
-  entriesText.appendChild(titles);
-
-  var entriesNotes = document.createElement('p');
-  entriesNotes.textContent = entry.notes;
-  entriesText.appendChild(entriesNotes);
-
-  return listofEntries;
-}
-/*
-Use a loop to create a DOM tree for each journal entry in the data model
-and append it to the page when the 'DOMContentLoaded' event is fired.
-*/
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  for (var i = 0; i < data.entries.length; i++) {
-    var entryData = viewEntries(data.entries[i]);
-    $entriesList.appendChild(entryData);
-  }
 });
