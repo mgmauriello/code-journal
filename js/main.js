9587b76bc1dc68228aabb8b6df5d68d01ff53ad4
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-16 10:44:16 -0800

    added anchor to entries navbar to bring user to view entries; click events for , , and entriesAnchor to move between pages of view entry and new entry. #2

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@
 /* exported data */
 var $input = document.querySelector('#photoUrl');
 var $img = document.querySelector('img');
-
 var $entryForm = document.querySelector('#entry-form');
 var $entrylist = document.querySelector('.entries-lists');
+var $newButton = document.querySelector('.new-button');
+var $saveButton = document.querySelector('.save-button');
+var $noEntry = document.querySelector('.no-entries-text');
+var entriesAnchor = document.querySelector('.entries');
+var newEntryContainer = document.querySelector('main');
+var $entriesViewContainer = document.querySelector('.entries-view-container');
 
 $input.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
@@ -28,6 +33,11 @@ $entryForm.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
 
   $entryForm.reset();
+
+  newEntryContainer.className = 'hidden';
+  $noEntry.className = 'hidden';
+  $entriesViewContainer.className = '';
+  data.view = 'entries';
 });
 
 // users can view their entry #2
@@ -68,4 +78,35 @@ document.addEventListener('DOMContentLoaded', function (event) {
     var entriesData = viewEntries(data.entries[i]);
     $entrylist.appendChild(entriesData);
   }
+
+  if (data.view === 'entries') {
+    newEntryContainer.className = 'hidden';
+  } else if (data.view === 'entry-form') {
+    $noEntry.className = 'hidden';
+    $entriesViewContainer.className = 'hidden';
+  }
+
+  if (data.entries.length === '') {
+    $noEntry.className = 'no-entries-text';
+  } else {
+    $noEntry.className = 'hidden';
+  }
+});
+
+$saveButton.addEventListener('click', function (event) {
+  $entriesViewContainer.className = '';
+  newEntryContainer.className = 'hidden';
+  data.view = 'entries';
+});
+
+$newButton.addEventListener('click', function (event) {
+  $entriesViewContainer.className = 'hidden';
+  newEntryContainer.className = '';
+  data.view = 'entry-form';
+});
+
+entriesAnchor.addEventListener('click', function (event) {
+  $entriesViewContainer.className = '';
+  newEntryContainer.className = 'hidden';
+  data.view = 'entries';
 });
