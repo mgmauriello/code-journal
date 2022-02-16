321a0316e4d941bc3cadd59fc282ebb1bb4f30ac
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-15 20:55:03 -0800

    Fixed errors with misspelling, removed unnescessary styling. #2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@
 /* exported data */
 var $input = document.querySelector('#photoUrl');
 var $img = document.querySelector('img');
-var $entriesList = document.querySelector('.entries-list');
+
 var $entryForm = document.querySelector('#entry-form');
-var $newButton = document.querySelector('.new- button');
-var $viewEntry = document.querySelector('[data-veiw="entries"]');
-var $noEntry = document.querySelector('.no-entries-text');
-var $newEntry = document.querySelector('[data-veiw="entry-form"]');
+var $entrylist = document.querySelector('.entries-lists');
 
 $input.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
@@ -15,34 +12,25 @@ $input.addEventListener('input', function (event) {
 
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  var entryId = {};
+  var newEntry = {};
 
-  entryId.photoUrl = $entryForm.elements.photoUrl.value;
-  entryId.title = $entryForm.elements.title.value;
-  entryId.notes = $entryForm.elements.notes.value;
+  newEntry.photoUrl = $entryForm.elements.photoUrl.value;
+  newEntry.title = $entryForm.elements.title.value;
+  newEntry.notes = $entryForm.elements.notes.value;
 
-  entryId.EntryId = data.newEntryID;
-  data.newEntryId++;
+  newEntry.newEntry = data.nextEntryId;
+  data.nextEntryId++;
 
-  data.entries.unshift(entryId);
-  var newEntry = viewEntries(entryId);
-  $entriesList.prepend(newEntry);
+  data.entries.unshift(newEntry);
+  var journalEntry = viewEntries(newEntry);
+  $entrylist.prepend(journalEntry);
 
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $entryForm.reset();
 
-  $newEntry.className = 'hidden';
-  $noEntry.className = 'hidden';
-  $viewEntry.className = '';
-  data.view = 'entries';
+  $entryForm.reset();
 });
 
-/*
-users can view their entry #2
-recreate entry form line by line (row (in div), col (in div), then img, divs
-form wont be form - title = h1? entries text = p ;; ^^ look at props
-in the $entrieslist - appendChild
-*/
+// users can view their entry #2
 
 function viewEntries(entry) {
   var listofEntries = document.createElement('li');
@@ -74,32 +62,10 @@ function viewEntries(entry) {
 
   return listofEntries;
 }
-/*
-Use a loop to create a DOM tree for each journal entry in the data model
-and append it to the page when the 'DOMContentLoaded' event is fired.
-*/
 
 document.addEventListener('DOMContentLoaded', function (event) {
   for (var i = 0; i < data.entries.length; i++) {
-    var entryData = viewEntries(data.entries[i]);
-    $entriesList.appendChild(entryData);
+    var entriesData = viewEntries(data.entries[i]);
+    $entrylist.appendChild(entriesData);
   }
-  if (data.view === 'entries') {
-    $newEntry.className = 'hidden';
-  } else if (data.view === 'entry-form') {
-    $noEntry.className = 'hidden';
-    $viewEntry.className = 'hidden';
-  }
-
-  if (data.entries.length === '') {
-    $noEntry.className = 'no-entries-text';
-  } else {
-    $noEntry.className = 'hidden';
-  }
-});
-
-$newButton.addEventListener('click', function (event) {
-  $viewEntry.className = 'hidden';
-  $newEntry.className = '';
-  data.view = 'entry-form';
 });
